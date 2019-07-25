b1232f599194978eb0db734d3a3dd3af81156ba7
Mark Jarvis <mtgjarvis@gmail.com>
2019-07-25 15:49:32 -0400

    Completed my first web app

--- my_first_web_app/urls.py
+++ my_first_web_app/urls.py
@@ -13,36 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from random import randint
-from django.http import HttpResponse
-from django.shortcuts import render
 from django.urls import path
-
-
-def home_page(request):
-    context = {'name': 'Mark Jarvis'}
-    response = render(request,'index.html', context)
-    return HttpResponse(response)
-
-def gallery_images(request):
-    image_urls = []
-    for i in range(6):
-        random_number = randint(0,100)
-        image_urls.append("https://picsum.photos/400/600/?image={}".format(random_number))
-    context = {'gallery_image': image_urls}
-    response = render(request, 'gallery.html', context)
-    return HttpResponse(response)
-
-def about_me(request):
-    context = {
-        'skills': ['Surfing', 'Coding', 'Cooking'],
-        'interests': ['Reading', 'Science', 'Politics', 'Art']
-        }
-    response = render(request, 'about_me.html', context)
-    return HttpResponse(response)
-
+from my_first_web_app.views import root, home_page, gallery_images, about_me
 
 urlpatterns = [
+    path('', root),
     path('home/', home_page),
     path('portfolio/', gallery_images),
     path('about_me/', about_me),
---
+++ my_first_web_app/views.py
@@ -0,0 +1,33 @@
+
+
+from random import randint
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+
+
+def home_page(request):
+    context = {'name': 'Mark Jarvis'}
+    response = render(request,'index.html', context)
+    return HttpResponse(response)
+
+def root(request):
+    return HttpResponseRedirect('home')
+
+def gallery_images(request):
+    image_urls = []
+    for i in range(6):
+        random_number = randint(0,100)
+        image_urls.append("https://picsum.photos/400/600/?image={}".format(random_number))
+    context = {'gallery_image': image_urls}
+    response = render(request, 'gallery.html', context)
+    return HttpResponse(response)
+
+def about_me(request):
+    context = {
+        'skills': ['Surfing', 'Coding', 'Cooking'],
+        'interests': ['Reading', 'Science', 'Politics', 'Art']
+        }
+    response = render(request, 'about_me.html', context)
+    return HttpResponse(response)
+
