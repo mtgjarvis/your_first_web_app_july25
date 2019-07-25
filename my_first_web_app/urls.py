4c7b64e895662fcfb0abefcbe14cb0d21078d818
Mark Jarvis <mtgjarvis@gmail.com>
2019-07-25 14:44:35 -0400

    added random image file to portfolio

--- my_first_web_app/urls.py
+++ my_first_web_app/urls.py
@@ -13,18 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from random import randint
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import path
 
 
 def home_page(request):
-    response = render(request,'index.html')
+    context = {'name': 'Mark Jarvis'}
+    response = render(request,'index.html', context)
     return HttpResponse(response)
 
 def gallery_page(request):
-    response = render(request, 'gallery.html')
+    random_number = randint(0,100)
+    image_url = "https://picsum.photos/400/600/?image={}".format(random_number)
+    context = {'gallery_image': image_url}
+    response = render(request, 'gallery.html', context)
     return HttpResponse(response)
 
 
